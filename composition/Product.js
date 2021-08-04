f79fd14ed84df92da8d31444dce706bac3daf5ef
Carlos <contact@carloscolin.me>
2021-08-04 17:17:58 -0500

    Implementing watchers on composition API

--- composition/Product.js
+++ composition/Product.js
@@ -20,7 +20,9 @@ app.component("product", {
             <badge :product="product" />
             <p class="description__status" v-if="product.stock === 3">Quedan pocas unidades!</p>
             <p class="description__status" v-else-if="product.stock === 2">Quedan muy poquitas unidades!</p>
-            <p class="description__price">$ {{ new Intl.NumberFormat("es-MX").format(product.price) }}</p>
+            <p class="description__price" :style="{ color: price_color }">
+                $ {{ new Intl.NumberFormat("es-MX").format(product.price) }}
+            </p>
             <p class="description__content">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit aut explicabo labore, error quibusdam atque maxime soluta excepturi debitis ex nostrum id sit minima perspiciatis nulla odio? Obcaecati, magni nesciunt.
             </p>
@@ -37,6 +39,7 @@ app.component("product", {
         const product_state = reactive({
             product: props.product,
             active_image: 0,
+            price_color: "rgb(104, 104, 209)",
         });
 
         const sendToCart = () => {
@@ -51,6 +54,21 @@ app.component("product", {
                 discount_codes.value.splice(discount_code_index, 1);
             }
         }
+
+        watch(
+            () => product_state.active_image,
+            (value, old_value) => {
+                console.log(value, old_value);
+        });
+
+        watch(
+            () => product_state.product.stock,
+            (value) => {
+                if (value <= 1) {
+                    product_state.price_color = "rgb(188, 30, 67)";
+                }
+            }
+        );
 
         return {
             ...toRefs(product_state),
